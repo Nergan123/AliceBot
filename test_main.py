1c3ef3b4051dcba5ba5f1aa77db7190aa5046869
Nergan123 <moskalenkovladimir1998@gmail.com>
2023-02-01 23:21:44 +0100

    Added interaction

--- test_main.py
+++ test_main.py
@@ -1,11 +1,9 @@
 import sys
 import pytest
 from tests.test_dice_roller import TestDiceRoller
-from tests.test_interaction_handler import TestInteractionHandler
 
 
 if __name__ == "__main__":
     sys.exit(pytest.main(["-qq"], plugins=[
-        TestDiceRoller(),
-        TestInteractionHandler()
+        TestDiceRoller()
     ]))
